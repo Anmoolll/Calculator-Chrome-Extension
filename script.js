2a38e6031e3f669a3cbd785d66ed8a8462d3e7bd
Anmoolll <anmol.agarwal2022@vitstudent.ac.in>
2024-04-25 22:32:41 +0530

    Version Update

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-// var stringMath = require('string-math');
+
 
 let string = ''
 let buttons = document.querySelectorAll('.button')
+
+function calculate(expression) {
+    let [a, operator, b] = expression.split(' ');
+    a = parseFloat(a);
+    b = parseFloat(b);
+    switch (operator) {
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/': return a / b;
+        default: throw new Error('Unknown operator');
+    }
+}
+
+
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click', (e)=>{
         if(e.target.innerHTML == '='){
-            string = eval(string);
+            string = calculate(string);
             document.querySelector('input').value = string;
         }else if(e.target.innerHTML == 'C'){
             string = ""
@@ -15,13 +30,16 @@ Array.from(buttons).forEach((button)=>{
             document.querySelector('input').value = string;
         }
         else{
-            console.log(e.target)
-            string = string + e.target.innerHTML;
+            if(['+', '-', '*', '/'].includes(e.target.innerHTML)){
+                string = string + ' ' + e.target.innerHTML + ' ';
+            }else{
+                string = string + e.target.innerHTML;
+            }
             document.querySelector('input').value = string;
         }
     })
 })
 
 
-
-// console.log(stringMath('2+2'));
+let result = calculate('2-1');
+console.log(result);
